6e20e8975258eb9a0e137f82c419dd264fd39988
xanhacks <xanhacks@protonmail.com>
2024-05-05 22:03:04 +0200

    update template

--- netcat/python/challenge.py
+++ netcat/python/challenge.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
-from pwn import xor
 from os import popen
 
 
 if __name__ == "__main__":
-    print("Hello world !")
-    print(xor(b"abc", b"aaaaaaaaaaaa"))
-
     print(popen("id").read())
     
     name = input("What's your name ? ")
--- netcat/python/entrypoint.sh
+++ netcat/python/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "Listening on port ${LISTEN_PORT}"
+
 while :
 do
-    su -c "exec socat TCP-LISTEN:${LISTEN_PORT},reuseaddr,fork EXEC:'/challenge/challenge.py,stderr'" - player;
+    su -c "exec socat TCP-LISTEN:${LISTEN_PORT},reuseaddr,fork EXEC:'/challenge/challenge.py,stderr'" - challenge;
 done
